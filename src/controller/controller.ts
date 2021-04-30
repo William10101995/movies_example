f42394cd7c9efd7eec207894aab91f8d34a7d0d2
William <lopezwilliam177@gmail.com>
2021-04-29 23:35:43 -0300

    Commit prueba

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -2,23 +2,23 @@ import { RequestHandler } from "express";
 import moviesClass from "../service/service";
 import Movie from "../models/movies";
 
-const intaceMovie = new moviesClass();
+const instaceMovie = new moviesClass();
 //Obtener Peliculas
 export const getMovies: RequestHandler = async (req, res) => {
-  res.send("Obteniendo Pelicula");
+
 };
 
 //Crear una nueva Pelicula
 export const createMovie: RequestHandler = async (req, res) => {
   const newMovie = new Movie(req.body);
-  const movie = await intaceMovie.createMovie(newMovie);
+  const movie = await instaceMovie.createMovie(newMovie);
   res.status(200).json(movie);
 };
 
 //Borrar una pelicula
 export const deleteMovie: RequestHandler = async (req, res) => {
   const idMovie = req.params;
-  const deleteMovie = await intaceMovie.deleteMovie(idMovie);
+  const deleteMovie = await instaceMovie.deleteMovie(idMovie);
   res.status(200).json(deleteMovie);
 };
 
@@ -26,6 +26,6 @@ export const deleteMovie: RequestHandler = async (req, res) => {
 export const updateMovie: RequestHandler = async (req, res) => {
   const idMovie = req.params;
   const paramsUpadate = req.body;
-  const MovieUpdate = await intaceMovie.updateMovie(idMovie, paramsUpadate);
+  const MovieUpdate = await instaceMovie.updateMovie(idMovie, paramsUpadate);
   res.status(200).json(MovieUpdate);
 };
